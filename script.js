0fd34e538ff1cd25bee1fdd0fd65e64028f8c9e0
Rafael Gonçalves <rafaelgonsalveslima21@gmail.com>
2021-03-17 14:58:11 -0300

    download

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ const images = document.querySelectorAll('#imgs')
 const modal = document.querySelector('.modal-img')
 const modalImg = document.querySelector('#img-modal')
 const btnClose = document.querySelector('#btn-close')
+const linkModal = document.querySelectorAll('#link-modal')
+const download = document.querySelector('#download')
 
 let srcVal = ''
+let hrefVal = ''
 
 for(let i =0; i<images.length;i++) {
   openModal[i].addEventListener('click', () => {
+
+    console.log(hrefVal = linkModal[i].getAttribute('href'))
+    download.setAttribute('href', hrefVal);
+
     console.log(srcVal = images[i].getAttribute('src'))
     modalImg.setAttribute('src', srcVal);
+
     modal.classList.add('modal-active')
   })
 }
